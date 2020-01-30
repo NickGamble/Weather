9550323ac1977962948c819a98b4f8ddd09314b7
Nick <nickgamble31@gmail.com>
2020-01-30 20:12:57 +0800

    Initial commit

---
+++ Weather.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weather.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
