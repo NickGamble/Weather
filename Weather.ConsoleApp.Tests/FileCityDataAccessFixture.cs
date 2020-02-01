236f298af26b2e5fefe6d5a1ca70c870397c42e0
Nick <nickgamble31@gmail.com>
2020-02-02 07:16:52 +0800

    FileCityAccess unit tests

---
+++ Weather.ConsoleApp.Tests/FileCityDataAccessFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.ConsoleApp.Tests
+{
+    public class FileCityDataAccessFixture
+    {
+
+        // TODO: Write some unit tests. Will need to abstract file access out to an interface
+    }
+}
